9e25a43730adead31d1af6007f1cbc2557175a1e
Alexander Ege <Alexander@Ege.com>
2019-11-24 14:49:18 -0600

    Intial Commit

---
+++ UserRegEON/Data/UserRegEONContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserRegEON.Models;
+
+namespace UserRegEON.Data
+{
+    public class UserRegEONContext : DbContext
+    {
+        public UserRegEONContext (DbContextOptions<UserRegEONContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserRegEON.Models.User> User { get; set; }
+    }
+}
---
+++ UserRegEON/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UserRegEON.Data;
+
+namespace UserRegEON.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new UserRegEONContext(
+                serviceProvider.GetRequiredService<DbContextOptions<UserRegEONContext>>()))
+            {
+                //Look for any Users
+                if (context.User.Any())
+                {
+                    return; //Database has been seeded
+                }
+
+                context.User.AddRange(
+                    new User
+                    {
+                        Name = "Alexander Ege",
+                        Email = "Alexander@Ege.com",
+                        Gender = "M",
+                        DateRegistered = DateTime.Parse("2019-24-10"),
+                        Days = "Day1, Day2",
+                        Requests = "None"
+                    },
+
+                    new User
+                    {
+                        Name = "Stephen Kaatz",
+                        Email = "Stephen@K.com",
+                        Gender = "M",
+                        DateRegistered = DateTime.Parse("2019-04-10"),
+                        Days = "Day1, Day2, Day3",
+                        Requests = "Private Room"
+                    },
+
+                    new User
+                    {
+                        Name = "Jane Doe",
+                        Email = "JaneDoe@gmail.com",
+                        Gender = "F",
+                        DateRegistered = DateTime.Parse("2019-21-10"),
+                        Days = "Day1",
+                        Requests = "Bringing a plus one."
+
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
